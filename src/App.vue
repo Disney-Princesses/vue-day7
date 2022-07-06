3c11733ac9a0ae17834b91997038b7730129192d
fengbujue <1125238900@qq.com>
2022-07-06 21:03:23 +0800

    完成了作用域插槽使用场景

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <Register></Register>
+    <!-- 手机邮箱注册切换案例 -->
+    <!-- <Register></Register> -->
+
+    <TableList></TableList>
   </div>
 </template>
 
 <script>
 import Register from './Register.vue'
+import TableList from './TableList.vue'
 export default {
   components: {
-    Register
+    Register,
+    TableList
   }
 }
 </script>
---
+++ src/components/Pannel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <table border="1" class="table">
+          <thead>
+              <tr>
+                  <th>序号</th>
+                  <th>姓名</th>
+                  <th>年龄</th>
+                  <th>头像</th>
+              </tr>
+          </thead>
+          <thead>
+              <tr v-for="obj in arr" :key="obj.id">
+                  <td>
+                    <slot name="userId" :row="obj">{{obj.id}}</slot>
+                  </td>
+                  <td>
+                    <slot name="userName" :row="obj">{{obj.name}}</slot>
+                  </td>
+                  <td>
+                    <slot name="userAge" :row="obj">{{obj.age}}</slot>
+                  </td>
+                  <td>
+                    <slot name="userImg" :row="obj"></slot>
+                  </td>
+              </tr>
+          </thead>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        arr: Array
+    }
+}
+</script>
