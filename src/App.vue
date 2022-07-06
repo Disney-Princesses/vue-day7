678981efcb383afcc5a73fbe6e5e2f4f8c7153a6
tianbingyao22 <2242378609@qq.com>
2022-07-06 20:32:25 +0800

    w

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,60 @@
 <template>
-  <div></div>
+  <div class="box">
+    <div class="tab">
+      <span :class="{ active: isSel == 'PhoneRegister' }" @click="clickFn1()"
+        >手机注册</span
+      >
+      <span :class="{ active: isSel == 'EmailRegister' }" @click="clickFn2()"
+        >邮箱注册</span
+      >
+    </div>
+    <keep-alive>
+      <component :is="comName"></component>
+    </keep-alive>
+  </div>
 </template>
 
 <script>
+import PhoneRegister from "./components/PhoneRegister.vue";
+import EmailRegister from "./components/EmailRegister.vue";
 export default {
-
-}
+  data() {
+    return {
+      comName: "PhoneRegister",
+      isSel: "PhoneRegister",
+    };
+  },
+  components: {
+    PhoneRegister,
+    EmailRegister,
+  },
+  methods: {
+    clickFn1() {
+      this.comName = "PhoneRegister";
+      this.isSel = "PhoneRegister";
+    },
+    clickFn2() {
+      this.comName = "EmailRegister";
+      this.isSel = "EmailRegister";
+    },
+  },
+};
 </script>
 
 <style>
-
+.box {
+  margin: 30px;
+}
+.tab {
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+span {
+  font-size: 20px;
+}
+.active {
+  color: #0043ed;
+  border-bottom: 3px solid #0043ed;
+}
 </style>
---
+++ src/components/PhoneRegister.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    
+    <p>选择所在地区(实名认证主体所在地)</p>
+    <input type="radio" v-model='contry' value="中国大陆">中国大陆
+    <input type="radio" v-model='contry' value="中国香港">中国香港
+    <br>
+    <br>
+    <p>手机号码</p>
+    <input type="text" placeholder="请输入手机号码">
+    <input type="text" placeholder="输入验证码">
+    <button>发送验证码</button>
+    <p>登陆密码</p>
+    <input type="text" placeholder="请输入登陆密码">
+    <input type="text" placeholder="再次输入登陆密码">
+    <p>邀请码(选填)</p>
+    <input type="text" placeholder='输入客户经理邀请码'>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+        contry:'',
+        phonenum:'',
+        code:"",
+        password:'',
+        invitacode:'',
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+p{
+  font-size: 12px;
+  color:#333;
+}
+input{
+  margin: 5px 0;
+}
+input[type='text']{
+  height: 30px;
+}
+button{
+  margin: 10px;
+  height: 35px;
+  background: #0d59f0;
+  color:#fff;
+  border:1px solid #0d59f0;
+  border-radius: 5px;
+}
+</style>
