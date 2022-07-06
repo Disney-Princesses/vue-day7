0cdc372eb3ded95dc46362b732d676d317a19b8d
GuanSzzz <847388139@qq.com>
2022-07-06 21:02:37 +0800

    完成了渲染页面

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <h1>注册表单</h1>
+    <h1>作业1:注册表单</h1>
     <inputS></inputS>
     <hr />
+    <h1>作业2:渲染页面</h1>
+    <tableS></tableS>
   </div>
 </template>
 
 <script>
 import inputS from "./views/input.vue";
+import tableS from "./views/tableT.vue";
 export default {
   components: {
     inputS,
+    tableS,
   },
 };
 </script>
---
+++ src/views/tableT.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <tableS>
+      <template v-slot:id="scope">
+        {{ scope.row.id }}
+      </template>
+      <template v-slot:name="scope">
+        {{ scope.row.name }}
+      </template>
+      <template v-slot:age="scope">
+        {{ scope.row.age }}
+      </template>
+      <template v-slot:toux="scope">
+        <img
+          v-if="scope.row.type == 1"
+          :src="scope.row.headImgUrl"
+          alt=""
+          width="40px"
+        />
+        <a v-else-if="scope.row.type == 0" href="scope.row.headImgUrl">{{
+          scope.row.headImgUrl
+        }}</a>
+        <span v-else-if="scope.row.type == 2">{{ scope.row.headImgUrl }}</span>
+      </template>
+    </tableS>
+  </div>
+</template>
+
+<script>
+import tableS from "../components/table.vue";
+export default {
+  components: {
+    tableS,
+  },
+};
+</script>
+
+<style></style>
